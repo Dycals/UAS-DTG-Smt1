12d08d775e548e6993bb70d64325b1e7addd830d
Dycals <70214014+Dycals@users.noreply.github.com>
2023-12-21 15:47:51 +0700

    Day 4

---
+++ UAS DTG/Assets/Scripts/Weapon/KnifeController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeController : WeaponController
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Attack()
+    {
+        base.Attack();
+        GameObject spawnedKnife = Instantiate(prefabs);
+        spawnedKnife.transform.position = transform.position;
+    }
+}
---
+++ UAS DTG/Assets/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    public GameObject prefabs;
+    public float damage;
+    public float speed;
+    public float cooldownDuration;
+    float currentCooldown;
+    public int pierce;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        currentCooldown = cooldownDuration;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        currentCooldown -= Time.deltaTime;
+        if(currentCooldown <= 0f )
+        {
+            Attack();
+        }
+    }
+
+    protected virtual void Attack()
+    {
+        currentCooldown = cooldownDuration;
+    }
+}
